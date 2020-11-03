cc26ad8d2e68791f98f911ec3f721ca1fa0b0585
yudai1202 <u.yudai1202@gmail.com>
2020-11-03 12:44:42 +0900

    fix: request same domein api path

--- src/libs/api/user.ts
+++ src/libs/api/user.ts
@@ -1,8 +1,9 @@
 import { IUserBody } from '@/libs/model/user';
 
 export const create = async (body: IUserBody) => {
+  const origin = location.origin;
   try {
-    const res = await fetch('http://localhost:3000/api/user', {
+    const res = await fetch(`${origin}/api/user`, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(body),
