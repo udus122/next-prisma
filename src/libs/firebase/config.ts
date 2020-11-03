6a920c35942a760b809720290b6c3df3f964e18f
yudai1202 <u.yudai1202@gmail.com>
2020-11-03 12:28:15 +0900

    refactor: modify how firebase config key import

--- src/libs/firebase/config.ts
+++ src/libs/firebase/config.ts
@@ -1,11 +1,9 @@
-import key from '../../../firebase-config-key.json';
-
 export const config = {
-  apiKey: key.apiKey,
-  authDomain: key.authDomain,
-  databaseURL: key.databaseURL,
-  projectId: key.projectId,
-  storageBucket: key.storageBucket,
-  messagingSenderId: key.messagingSenderId,
-  appId: key.appId,
+  apiKey: process.env.NEXT_PUBLIC_apiKey,
+  authDomain: process.env.NEXT_PUBLIC_authDomain,
+  databaseURL: process.env.NEXT_PUBLIC_databaseURL,
+  projectId: process.env.NEXT_PUBLIC_projectId,
+  storageBucket: process.env.NEXT_PUBLIC_storageBucket,
+  messagingSenderId: process.env.NEXT_PUBLIC_messagingSenderId,
+  appId: process.env.NEXT_PUBLIC_appId,
 };
